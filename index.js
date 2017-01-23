f915d9fe1e93bc7a45d0260b51cbf221c134b35a
etiennedilocker <etienne@dilocker.de>
2017-01-24 00:10:34 +0100

    create waitForElement, add some tests

---
+++ index.js
@@ -0,0 +1,42 @@
+import AssertionError from 'assertion-error';
+
+const DISPLAY_NAME = 'waitForElement';
+
+export const createWaitForElement = (selector, maxTime = 2000, interval = 10) => rootComponent => {
+
+  // Check correct usage
+  if (!selector) {
+    return Promise.reject(new AssertionError(`No selector specified in ${DISPLAY_NAME}.`));
+  }
+
+  if (!rootComponent) {
+    return Promise.reject(new AssertionError(`No root component specified in ${DISPLAY_NAME}.`));
+  }
+
+  if (!rootComponent.length) {
+    return Promise.reject(new AssertionError(`Specified root component in ${DISPLAY_NAME} not found.`));
+  }
+
+
+  // Race component search against maxTime
+  return new Promise((resolve, reject) => {
+
+    let remainingTime = maxTime;
+
+    const intervalId = setInterval(() => {
+      if (remainingTime < 0) {
+        clearInterval(intervalId);
+        return reject(new AssertionError(`Expected to find ${selector} within ${maxTime}ms, but it was never found.`))
+      }
+
+      const targetComponent = rootComponent.find(selector);
+      if (targetComponent.length) {
+        clearInterval(intervalId);
+        return resolve(targetComponent);
+      }
+
+      remainingTime = remainingTime - interval;
+    }, interval)
+  });
+};
+
---
+++ index.spec.js
@@ -0,0 +1,82 @@
+import { createWaitForElement } from './index'
+
+const wait = createWaitForElement('mockSelector');
+
+describe('wrong usage', () => {
+  it('throws an Assertion error if no root component is specified.', () => {
+    wait(undefined)
+      .catch(err => expect(err).toMatchSnapshot('no component'))
+  });
+
+  it('throws an Assertion error if a root component cannot be found', () => {
+    const mockComponent = {
+      length: 0,
+    };
+
+    wait(mockComponent)
+      .catch(err => expect(err).toMatchSnapshot('component length zero'))
+  });
+
+  it('throws an Assertion error if no selector is specified', () => {
+
+    const wait = createWaitForElement(undefined);
+    wait()
+      .catch(err => expect(err).toMatchSnapshot('no selector'))
+  });
+});
+
+describe('core logic', () => {
+  describe('target component always ready', () => {
+
+    let mockComponent;
+
+    beforeEach(() => {
+      mockComponent = {
+        find: jest.fn().mockReturnValue({ length: 1 }),
+        length: 1,
+      }
+    });
+
+    it('resolves after calling find on the root component with the selector', () => {
+      return wait(mockComponent)
+        .then(() => {
+          expect(mockComponent.find).toHaveBeenCalledWith('mockSelector')
+        })
+    })
+  });
+
+  describe('target component never ready', () => {
+
+    let mockComponent;
+
+    beforeEach(() => {
+      mockComponent = {
+        find: jest.fn().mockReturnValue({ length: 0 }),
+        length: 1,
+      }
+    });
+
+    it('rejects after calling find on the root component with the selector', () => {
+      return wait(mockComponent)
+        .catch(() => {
+          expect(mockComponent.find).toHaveBeenCalledWith('mockSelector')
+        })
+    });
+
+    it('rejects after calling find on the root component 201 times', () => {
+      // one initial call, then 200 calls (2000ms / 10ms = 200 times)
+
+      return wait(mockComponent)
+        .catch(()=> {
+          expect(mockComponent.find).toHaveBeenCalledTimes(201)
+        })
+    });
+
+    it('rejects after calling find on the root component with the selector', () => {
+      return wait(mockComponent)
+        .catch(e => {
+          expect(e).toMatchSnapshot('run into timeout')
+        })
+    })
+  })
+});
