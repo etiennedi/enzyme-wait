bbd400091a14c2937471608605b0acc51a4e8594
etiennedi <etienne@dilocker.de>
2017-03-24 19:05:10 +0100

    add empty example repo

---
+++ example/src/Example.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Example =  () => (
+  <div>
+    <h1>Hello world!</h1>
+    <p>
+      TODO: do something async here
+    </p>
+  </div>
+);
+
+export default Example;
